ffe7ba2abba78475aab3cf53a238b54d44b24be3
WithLei <syunki@yeah.net>
2018-12-18 21:32:09 +0800

    12.18 Toilet类

--- toilet-intelligent-management/src/cn/javaee/dao/daoimpl/ToiletDAOImpl.java
+++ toilet-intelligent-management/src/cn/javaee/dao/daoimpl/ToiletDAOImpl.java
@@ -1,47 +1,123 @@
 package cn.javaee.dao.daoimpl;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.javaee.bean.Position;
 import cn.javaee.bean.Toilet;
+import cn.javaee.bean.User;
 import cn.javaee.dao.dao.ToiletDAO;
 
 public class ToiletDAOImpl extends BaseDAOImpl implements ToiletDAO{
 
 	@Override
 	public boolean save(Toilet entity) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "insert into toilet(name, type,cleanerid,last_clanned_time,next_clanned_time,isService,floorid) value(?,?,?,?,?,?,?)";
+		try (Connection connection =  ds.getConnection();
+			 PreparedStatement ps = connection.prepareStatement(sql)) {
+			 ps.setString(1, entity.getName());
+			 ps.setInt(2, entity.getType());
+			 ps.setInt(3, entity.getCleaner().getId());
+			 ps.setTimestamp(4,null);
+			 ps.setTimestamp(5,null);
+			 ps.setBoolean(6, entity.isService());
+		   //ps.setInt(7, entity.getFloor.getId());  楼层项写的有点问题。
+			 ps.executeUpdate();
+				return true;
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
+		String sql = "delete from toilet where id=\"" + id + "\"";
 		return false;
 	}
 
 	@Override
 	public boolean update(Toilet entity) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "UPDATE toilet SET name=?,type=?,cleanerid=?,last_clanned_time=?,next_clanned_time=?,isService=?,floorid=? "
+				+ "WHERE id=?";
+		try (Connection connection = ds.getConnection();
+				PreparedStatement ps = connection.prepareStatement(sql)) {
+		    ps.setString(1, entity.getName());
+			ps.setInt(2, entity.getType());
+			ps.setInt(3, entity.getCleaner().getId());
+			ps.setDate(4, (Date) entity.getLast_cleaned_time());
+			ps.setDate(5,(Date) entity.getNext_clean_time());
+			ps.setBoolean(6, entity.isService());
+		  //ps.setInt(7, entity.getFloor.getId());  楼层项写的有点问题。
+			ps.executeUpdate();
+			return true;
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-
+	
 
 	@Override
 	public Toilet getById(int id) {
-		// TODO Auto-generated method stub
+		String sql = "select * from toilet where id=" + id;
+		try (Connection conn = ds.getConnection();
+	             PreparedStatement ps = conn.prepareStatement(sql);
+	             ResultSet rs = ps.executeQuery()) {
+	            if (rs.next()) {
+	                return getToiletInSql(rs);
+	            }
+	        }catch (SQLException e) {
+	            e.printStackTrace();
+	            return null;
+	        }
 		return null;
 	}
+	
 
-	@Override
+	
+
+	private Toilet getToiletInSql(ResultSet rs) throws SQLException {
+    	Toilet toilet = new Toilet();
+    	toilet.setId(rs.getInt(1));
+    	toilet.setName(rs.getString(2));
+        return toilet;
+    }
+
 	public List<Toilet> getById(int[] ids) {
+		
 		// 不做
 		return null;
 	}
 
 	@Override
 	public List<Toilet> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String sql = "select * from toilet";
+		
+		return getToiletList(sql);
+		
+	}
+
+	private List<Toilet> getToiletList(String sql) {
+		List<Toilet>toilets = new ArrayList<>();
+		
+		try(Connection conn = ds.getConnection();
+	             PreparedStatement ps = conn.prepareStatement(sql);
+	             ResultSet rs = ps.executeQuery()) {
+			while(rs.next()) {
+				toilets.add(getToiletInSql(rs));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return toilets;
 	}
 
 }
---
+++ toilet-intelligent-management/src/cn/javaee/servlet/toilet/CommonServlet.java
@@ -0,0 +1,61 @@
+package cn.javaee.servlet.toilet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import cn.javaee.bean.Toilet;
+import cn.javaee.dao.daoimpl.ToiletDAOImpl;
+import cn.javaee.servlet.BaseHttpServlet;
+
+
+@WebServlet({"/getAllToilet","/getToiletById","/saveToilet"})
+public class CommonServlet extends BaseHttpServlet {
+
+	@Override
+	public void doMyGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uri = request.getRequestURI();
+		
+		if (uri.endsWith("getAllToilet")) {
+			ToiletDAOImpl toiletDAOImpl = new ToiletDAOImpl();
+			List<Toilet>toilets = toiletDAOImpl.getAll();
+			
+			Gson gson = new Gson();
+			String json = gson.toJson(toilets);
+			PrintWriter out = new PrintWriter(response.getOutputStream());
+			out.print(json);
+			out.flush();
+		}
+		if (uri.endsWith("getToiletById")) {
+			String id=request.getParameter("id");
+			ToiletDAOImpl toiletDAOImpl = new ToiletDAOImpl();
+			Toilet userid = toiletDAOImpl.getById(Integer.parseInt(id));
+			
+			Gson gson = new Gson();
+			String json = gson.toJson(userid);
+			PrintWriter out = new PrintWriter(response.getOutputStream());
+			out.print(json);
+			out.flush();
+		}
+		
+		
+        if (uri.endsWith("saveToilet")) {
+        	ToiletDAOImpl toiletDAOImpl = new ToiletDAOImpl();
+			Toilet saveid = toiletDAOImpl.getById(3);
+			
+			Gson gson = new Gson();
+			String json = gson.toJson(saveid);
+			PrintWriter out = new PrintWriter(response.getOutputStream());
+			out.print(json);
+			out.flush();
+		}
+
+	}
+}
